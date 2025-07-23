ec14a0ea883cdcde7d810822f5a7a01990a95df8
Hershi Shor <e0548597918@gmail.com>
2025-07-23 16:26:20 +0300

    fixed update prameter for update

--- src/routes/playerRouter.js
+++ src/routes/playerRouter.js
@@ -37,7 +37,7 @@ router.post('/',async (req, res) => {
 
 router.put('/',async (req, res) => {
     try{
-        const action = await playerCrud.updatePlayer(req.body.filter, req.body.updated)
+        const action = await playerCrud.updatePlayer(req.body.filter, req.body.update)
         res.status(201).send(action);
     }catch(err){
         res.status(err.status || 500).json({error : err.message} || {error : "Server internal error!"});
