e7721f7de2fbb032bebc4706a25b87f120813056
Hershi Shor <e0548597918@gmail.com>
2025-09-03 17:43:48 +0300

    added token and connect to front end

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
 import riddleRouter from "./src/routes/riddleRouter.js";
 import playerRouter from "./src/routes/playerRouter.js";
 import userRouter from "./src/routes/authRouter.js";
@@ -9,14 +10,23 @@ import cookieParser from "cookie-parser";
 
 const PORT = 5000;
 const server = express();
+server.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://riddle-game-web.netlify.app",
+    ],
+    credentials: true,
+  })
+);
 server.use(cookieParser());
 server.use(express.json());
 
-server.use('/users', userRouter);
-server.use('/riddles', riddleRouter);
-server.use('/players', playerRouter);
-
+server.use("/users", userRouter);
+server.use("/riddles", riddleRouter);
+server.use("/players", playerRouter);
 
 server.listen(PORT, () => {
-    console.log(`listening to port ${PORT}`);
+  console.log(`listening to port ${PORT}`);
 });
