0ea666db84f4a47f7f35d94d19563a86ec5525ba
Hershi Shor <e0548597918@gmail.com>
2025-09-04 14:56:12 +0300

    added a validate for token

--- middleWare/auth.js
+++ middleWare/auth.js
@@ -1,16 +1,24 @@
 import jwt from "jsonwebtoken";
 
 const auth = (rolse) => (req, res, next) => {
-    try{
-        const token = req.cookies.token;
-        if (!token) return res.status(403).send('Unauthorized');
-        const decoded = jwt.verify(token , process.env.SECRET);
-        const user = {id : decoded.id};
-        if (!rolse.includes(decoded.role)) return res.status(403).send('Unauthorized');
-        next();
-    }catch(error){
-        res.status(500).send(error.message);
-    };
+  try {
+    const headerToken = req.headers["authorization"];
+    if (!headerToken) return res.status(403).send("Unauthorized");
+
+    const token = headerToken.split(" ")[1];
+    if (!token) return res.status(403).send("Unauthorized");
+
+    const decoded = jwt.verify(token, process.env.SECRET);
+
+    if (!rolse.includes(decoded.role)) {
+      return res.status(403).send("Unauthorized");
+    }
+
+    req.user = { id: decoded.id, role: decoded.role };
+    next();
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
 export default auth;
